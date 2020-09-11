43c5618ea8cd4d3993eb68aedb0a5ecf582bed8d
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-09-10 20:37:22 -0400

    able to see my coordinates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
+    const [state, setState] = useState({
+        longitude: 0,
+        latitude: 0
+    });
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            function (position){
+                setState({
+                    longitude: position.coords.longitude,
+                    latitude: position.coords.latitude
+                })
+            },
+            function(error){
+                console.log(error);
+            },
+            {
+                enableHighAccuracy: true
+            }
+        )
+    })
     return (
         <div>
-            home
+            <h1>geolocation</h1>
+            <p>longitude: {state.longitude}</p>
+            <p>latitude: {state.latitude}</p>
         </div>
     )
 }
 
-export default Home
+export default Home;
