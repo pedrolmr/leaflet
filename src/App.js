de4d953c040c6ab74f58fe9b031c0fc3532a15eb
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-09-10 20:26:26 -0400

    added a route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import MapView from './components/MapView';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <MapView />
-    </div>
+    // <Router>
+      <Switch>
+        <Route path='/map'>
+          <MapView />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    // </Router>
   );
 }
 
